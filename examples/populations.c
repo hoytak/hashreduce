c1d13fcfa830092caabb14941a2551c87a0661b8
Lucas Koepke <lionrider@gmail.com>
2011-10-09 21:44:16 -0700

    Added a switch command to allow choosing which algorithm to speed test, 2, 3, 4 or 5.  Added rough code to Algorithms 4 and 5, though not working.

--- examples/populations.c
+++ examples/populations.c
@@ -19,12 +19,14 @@
 void generatePopulation(int Ni, int Nr, unsigned int seed, HashTable* population);
 void Algorithm2(int Ni, int Nr, unsigned int seed);
 void Algorithm3(int Ni, int Nr, unsigned int seed);
+void Algorithm4(int Ni, int Nr, unsigned int seed);
+void Algorithm5(int Ni, int Nr, unsigned int seed);
 
-int main()
+int main(int argc, char **argv)
 {
     double start, elapsed;
 
-    int i, j, k;
+    int i, j, k, choice;
     int Ni[5], Nr[6];
 
     double results[5][7];
@@ -34,64 +36,143 @@ int main()
     Ni[0] = 1000; Ni[1] = 10000; Ni[2] = 100000; Ni[3] = 1000000;
     Nr[0] = 1; Nr[1] = 10; Nr[2] = 100; Nr[3] = 1000; Nr[4] = 10000;
 
-    printf("\nTesting Algorithm 2 ... \n\n");
+    if(argc == 2){
+	choice = atoi(argv[1]);
+    }
 
-    results[0][0] = 0.0;
+    if(argc !=2){
+	printf("Incorrect arguments.  Please specify algorithm number to test:\n");
+	printf("Algorithm choices are 2, 3, 4, or 5\n");
+	printf("Example: ./population_example 2\n");
+    }
 
-    for(i=0; i<4; i++){
-	results[i+1][0] = (double)Ni[i];
-	for(j=0; j<5; j++){
-	    results[0][j+1] = (double)Nr[j];
-	    start = (double)clock();
-	    for(k=0;k<10;k++){
-		Algorithm2(Ni[i], Nr[j], seed);
+    switch(choice)
+    {
+    case 2:
+
+	printf("\nTesting Algorithm 2 ... \n\n");
+
+	results[0][0] = 0.0;
+
+	for(i=0; i<4; i++){
+	    results[i+1][0] = (double)Ni[i];
+	    for(j=0; j<5; j++){
+		results[0][j+1] = (double)Nr[j];
+		start = (double)clock();
+		for(k=0;k<10;k++){
+		    Algorithm2(Ni[i], Nr[j], seed);
+		}
+		elapsed = ((double)clock() - start)/CLOCKS_PER_SEC;
+		printf("%lf\t%d\t%d\n", elapsed/10, Ni[i], Nr[j]);
+		results[i+1][j+1] = elapsed;
 	    }
-	    elapsed = ((double)clock() - start)/CLOCKS_PER_SEC;
-	    printf("%lf\t%d\t%d\n", elapsed/10, Ni[i], Nr[j]);
-	    results[i+1][j+1] = elapsed;
+	    printf("\n");
 	}
-	printf("\n");
-    }
 
 
-    printf("\nResults for Algorithm 2: \n\n");
+	printf("\nResults for Algorithm 2: \n\n");
 
-    for(i=0; i<5; i++){
-	for(j=0; j<6; j++){
-	    printf("%lf\t", results[i][j]);
+	for(i=0; i<5; i++){
+	    for(j=0; j<6; j++){
+		printf("%lf\t", results[i][j]);
+	    }
+	    printf("\n");
 	}
-	printf("\n");
-    }
+	break;
     
-    printf("\nTesting Algorithm 3 ... \n\n");
+    case 3:
+	printf("\nTesting Algorithm 3 ... \n\n");
+	
+	results[0][0] = 0.0;
 
-    results[0][0] = 0.0;
-
-    for(i=0; i<4; i++){
-	results[i+1][0] = (double)Ni[i];
-	for(j=0; j<5; j++){
-	    results[0][j+1] = (double)Nr[j];
-	    start = (double)clock();
-	    for(k=0;k<10;k++){
-		Algorithm3(Ni[i], Nr[j], seed);
+	for(i=0; i<4; i++){
+	    results[i+1][0] = (double)Ni[i];
+	    for(j=0; j<5; j++){
+		results[0][j+1] = (double)Nr[j];
+		start = (double)clock();
+		for(k=0;k<10;k++){
+		    Algorithm3(Ni[i], Nr[j], seed);
+		}
+		elapsed = ((double)clock() - start)/CLOCKS_PER_SEC;
+		printf("%lf\t%d\t%d\n", elapsed/10, Ni[i], Nr[j]);
+		results[i+1][j+1] = elapsed;
 	    }
-	    elapsed = ((double)clock() - start)/CLOCKS_PER_SEC;
-	    printf("%lf\t%d\t%d\n", elapsed/10, Ni[i], Nr[j]);
-	    results[i+1][j+1] = elapsed;
+	    printf("\n");
 	}
-	printf("\n");
+
+	printf("\nResults for Algorithm 3: \n\n");
+
+	for(i=0; i<5; i++){
+	    for(j=0; j<6; j++){
+		printf("%lf\t", results[i][j]);
+	    }
+	    printf("\n");
+	}
+	break;
+
+    case 4:
+	printf("\nTesting Algorithm 4 ... \n\n");
+	
+	results[0][0] = 0.0;
+
+	for(i=0; i<4; i++){
+	    results[i+1][0] = (double)Ni[i];
+	    for(j=0; j<5; j++){
+		results[0][j+1] = (double)Nr[j];
+		start = (double)clock();
+		for(k=0;k<10;k++){
+		    Algorithm4(Ni[i], Nr[j], seed);
+		}
+		elapsed = ((double)clock() - start)/CLOCKS_PER_SEC;
+		printf("%lf\t%d\t%d\n", elapsed/10, Ni[i], Nr[j]);
+		results[i+1][j+1] = elapsed;
+	    }
+	    printf("\n");
+	}
+
+	printf("\nResults for Algorithm 4: \n\n");
+
+	for(i=0; i<5; i++){
+	    for(j=0; j<6; j++){
+		printf("%lf\t", results[i][j]);
+	    }
+	    printf("\n");
+	}
+	break;
+
+    case 5:
+	printf("\nTesting Algorithm 5 ... \n\n");
+	
+	results[0][0] = 0.0;
+
+	for(i=0; i<4; i++){
+	    results[i+1][0] = (double)Ni[i];
+	    for(j=0; j<5; j++){
+		results[0][j+1] = (double)Nr[j];
+		start = (double)clock();
+		for(k=0;k<10;k++){
+		    Algorithm5(Ni[i], Nr[j], seed);
+		}
+		elapsed = ((double)clock() - start)/CLOCKS_PER_SEC;
+		printf("%lf\t%d\t%d\n", elapsed/10, Ni[i], Nr[j]);
+		results[i+1][j+1] = elapsed;
+	    }
+	    printf("\n");
+	}
+
+	printf("\nResults for Algorithm 5: \n\n");
+
+	for(i=0; i<5; i++){
+	    for(j=0; j<6; j++){
+		printf("%lf\t", results[i][j]);
+	    }
+	    printf("\n");
+	}
+	break;
+    default:
+	printf("\nError: check options specified.\n\n");
     }
 
-
-    printf("\nResults for Algorithm 3: \n\n");
-
-    for(i=0; i<5; i++){
-	for(j=0; j<6; j++){
-	    printf("%lf\t", results[i][j]);
-	}
-	printf("\n");
-    }
- 
     return 0;
 }
 
@@ -196,3 +277,49 @@ void Algorithm3(int Ni, int Nr, unsigned int seed){
 
     return;
 }
+
+
+void Algorithm4(int Ni, int Nr, unsigned int seed){
+
+    HashTable* population1 = NewHashTable();
+
+    generatePopulation(Ni, Nr, seed, population1);
+
+    int NUM = 100000;
+
+    LCGState rng_state = Lcg_New(seed);
+
+    int t = (int)(Lcg_Next(&rng_state) % NUM); /* random time t */
+
+    obj_ptr hash = HashAtMarker(population1, t);
+
+    vset_ptr equality_vset = EqualityVSet(population1, hash);
+    
+    O_DECREF(population1);
+    O_DECREF(equality_vset);
+
+    return;
+}
+
+
+void Algorithm5(int Ni, int Nr, unsigned int seed){
+
+    
+    HashTable* population1 = NewHashTable();
+    HashTable* population2 = NewHashTable();
+
+    generatePopulation(Ni, Nr, seed, population1);
+    generatePopulation(Ni, Nr, seed+1, population2);
+
+    HashTable* keys = KeySet(population2);   
+
+    mset_ptr equality_mset = MSetDifference(population1, keys);
+
+    O_DECREF(population1);
+    O_DECREF(population2);
+    O_DECREF(equality_mset);
+
+    return;
+}
+
+
